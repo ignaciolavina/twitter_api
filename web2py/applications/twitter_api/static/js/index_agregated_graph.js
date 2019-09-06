09f37ceea17665639ff04354816dd15e342a8597
lavinaIgnacio <ucone92@gmail.com>
2019-09-06 02:33:53 +0200

    joined everything to be able to show and get feedback

--- web2py/applications/twitter_api/static/js/index_agregated_graph.js
+++ web2py/applications/twitter_api/static/js/index_agregated_graph.js
@@ -1,83 +1,3 @@
-let on_page_load = function () {
-    // get_tweet();
-    // console.log('action');
-
-    // $.post(logginTwitterURL, {
-    //     word: 'tweet'
-    // }, function (response) {
-
-    // });
-};
-
-let test_btn = function () {
-    console.log("test btn pressed");
-    $.post(testBtnURL, {
-    }, function (response) {
-
-    });
-}
-
-let action = function () {
-    console.log('post tweet function');
-
-    $.post(postTweetURL, {
-        string: 'third tweet'
-    }, function (response) {
-
-    });
-}
-
-let get_tweet = function () {
-    console.log('get tweet function');
-    // app.label = app.word_search;
-    $.post(getTweetURL, {
-        word: app.word_search
-    }, function (response) {
-        console.log(response);
-        // app.label = response.tweet;
-        // app.label = JSON.stringify(response.tweet, null, "\t");
-        // console.log(response.tweet);
-        app.tweets = response.tweets;
-        app.retweets = response.retweets;
-
-        app.data_loaded = true;
-        // THis function is in index_agregated_graph.js file
-        get_data_for_graphs();
-
-        get_top_users();
-        // document.body.appendChild(document.createTextNode(JSON.stringify(response.tweet, null, 4)));
-        document.body.appendChild(document.createTextNode(JSON.stringify(app.retweets, null, 4)));
-    });
-}
-
-
-let get_top_users = function () {
-    for (let i = 0; i < 10; i++) {
-        app.top_users.push(app.tweets[0].user);
-    }
-};
-
-let app = new Vue({
-    el: "#index",
-    delimiters: ['${', '}'],
-    unsafeDelimiters: ['!{', '}'],
-    data: {
-        label: 'none',
-        data_loaded: false,
-        word_search: '',
-        tweets: [],
-        retweets: [],
-        top_users: []
-    },
-    methods: {
-        action: action,
-        get_tweet: get_tweet,
-        test_btn: test_btn
-    }
-});
-
-
-
 
 let get_data_for_graphs = function () {
     console.log('creating data');
@@ -100,7 +20,6 @@ let get_data_for_graphs = function () {
 
 
 
-var data = [];
 
 // var timeFormat = 'YYYY-MM-DDTHH:mm:ss.sssZ';
 var timeFormat = "ddd MMM dd HH:mm:ss Z yyyy"
@@ -174,8 +93,3 @@ var config = {
 // window.onload = function () {
 
 // };
-
-
-
-on_page_load();
-
--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b3b7b23f1212b0b2$c3b82b613df400ddb915a5d9dc50c06bf6d1d254"
+password="pbkdf2(1000,20,sha512)$b0c8255c1b259f0e$8224bb5d939b12afdb30d6f1e99c997f4928faa6"
