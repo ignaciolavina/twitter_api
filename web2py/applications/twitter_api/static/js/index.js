6df43b980e531ff9bdcc1d97c1d5f66fb3a3bbbf
lavinaIgnacio <ucone92@gmail.com>
2019-09-21 23:28:02 +0200

    Managed to display the multiple-line graph. Fixed agregated-graph Code Refactoring

--- web2py/applications/twitter_api/static/js/index.js
+++ web2py/applications/twitter_api/static/js/index.js
@@ -1,58 +1,59 @@
+// Fixes and to do list
+// - word_search is defined as 'casa' for testing purposes. set to ''
+// - when refreshing the page, clear all the variables and arrays array= []
+// - Check if save data is working, it seems to be storing[Object(object)], etc
+// - On EACH server response, check server response and display errors
+// - Check also if server response is empty
+// - Implement 'topic' on mark as fake and store data function
+// - Improve Colors, labels, fillments, and other graph properties
+// - Check in chart.js documentation if there are more interesting properties to put on "config" for the graphs
+
+// TO-DO list
+// - Imlpement tables:
+//     - top fake spreaders 10 que mas han repercutido
+//     - top first users: 10 primeros
+//     - top potential viewers users: top 10 users con mas followers (mas potential scope)
+// - Download libraries instead of fetching them online
+
+
 let on_page_load = function () {
-    // console.log(response);
-    // console.log(request.args(0));
+    // To get retrieve data
     console.log('************url******');
     console.log(window.location.href);
     console.log('************End******');
-    // console.log('route', this.$router.params);
-    // get_tweet();
+
+    // Only for testing purposes
+    get_tweet();
 };
 
-let test_btn = function () {
-    console.log("test btn pressed");
-    $.post(testBtnURL, {
-    }, function (response) {
-
-    });
-}
-
-let action = function () {
-    console.log('post tweet function');
-    $.post(postTweetURL, {
-        string: 'third tweet'
-    }, function (response) {
-
-    });
-}
 
 let save_data = function () {
-    console.log('save data');
-    console.log(app.list_tweets_and_retweets);
-    variable = app.list_tweets_and_retweets.join();
+    console.log('save data function');
     $.post(saveDataURL, {
-        stored_data: variable
+        stored_data: app.list_tweets_and_retweets.join()
     }, function (response) {
-        console.log('server response')
+        // console.log('server response');
         if (response.result == true) {
-            console.log('data saved');
+            console.log('data saved corretly');
         } else {
             window.alert(response.error);
         }
     });
 }
 
+
 let get_tweet = function () {
     console.log('get tweet function');
-    // app.label = app.word_search;
     $.post(getTweetURL, {
         word: app.word_search
     }, function (response) {
         console.log('server response')
-        console.log(response);
+        // console.log(response);
 
         // Empty the varlue in case of refresh or new data comming
         app.tweets = [];
         app.retweets = [];
+
         // Retrieveing the data from request vars
         app.list_tweets_and_retweets = response.list_tweets_and_retweets;
         for (let i = 0; i < app.list_tweets_and_retweets.length; i++) {
@@ -62,28 +63,23 @@ let get_tweet = function () {
 
         app.data_loaded = true;
         // This function is in index_agregated_graph.js file
-        get_data_for_graphs();
+        create_tables();
+        create_graphs();
 
-        get_top_users();
         // document.body.appendChild(document.createTextNode(JSON.stringify(response.tweets, null, 4)));
         document.body.appendChild(document.createTextNode(JSON.stringify(app.list_tweets_and_retweets, null, 4)));
-
     });
 }
 
 
 let mark_as_fake = function () {
-    console.log('mark as fake new');
+    console.log('mark as fake function');
     stored_data = app.list_tweets_and_retweets.join();
     $.post(markFakeURL, {
         search_line: app.word_search,
         topic: '',
         stored_data: stored_data
-
-        // word: app.word_search
-
     }, function (response) {
-        console.log('server response')
         if (response.result == true) {
             console.log('data saved');
             // window.alert('data saved');
@@ -93,6 +89,12 @@ let mark_as_fake = function () {
     });
 }
 
+let create_tables = function () {
+    get_top_users();
+}
+
+
+// ______________ GRAPHS AND LIST COMMON METHODS ______________
 
 let get_top_users = function () {
     for (let i = 0; i < 10; i++) {
@@ -100,63 +102,35 @@ let get_top_users = function () {
     }
 };
 
-let app = new Vue({
-    el: "#index",
-    delimiters: ['${', '}'],
-    unsafeDelimiters: ['!{', '}'],
-    data: {
-        label: 'none',
-        data_loaded: true,
-        word_search: 'casa',
-        tweets: [],
-        retweets: [],
-        top_users: [],
-        list_tweets_and_retweets: [],
-        agregated_retweets: [],
-        mark_as_fake: mark_as_fake
-    },
-    methods: {
-        action: action,
-        get_tweet: get_tweet,
-        test_btn: test_btn,
-        save_data: save_data
-    }
-});
-
-
-// ________________________________________________________________
-// For agreggated graphic
-
-var data = [];
-
-
-let get_data_for_graphs = function () {
-
-    //Creation of the agregated graph
+let create_graphs = function () {
     create_agregated_graph();
     create_multiple_graph();
 }
 
 
+
+// ____________________ AGREGATED GRAPH _______________________
+
+var data = [];
+// var timeFormat = 'YYYY-MM-DDTHH:mm:ss.sssZ';
+var timeFormat = "ddd MMM dd HH:mm:ss Z yyyy"
+
 let create_agregated_graph = function () {
 
-    console.log('creating data for agregated graph');
-    let counter = 1;
-
-    // Creation of a list that will agregate all the agregated retweets
-    let agregate_rt = [];
+    // Joining all the retweets from different tweets in a single list "app.agregated_retweets"
+    app.agregated_retweets = [];
     for (let i = 0; i < app.retweets.length; i++) {
-        app.agregated_retweets = agregate_rt.concat(app.retweets[i]);
+        app.agregated_retweets = app.agregated_retweets.concat(app.retweets[i]);
     }
 
-    // Date sorting to display the agregated graph
+    // Sorting the list according to Data (to display it in order on the agregated graph)
     app.agregated_retweets.sort(function (a, b) { return (new Date(a.created_at) - new Date(b.created_at)).toString() });
 
-    // app.agregated_retweets.sort(function (a, b) => new Date(a.created_at) - new Date(b.created_at));
+    let counter = 0;
+    // Agregating each retweet to the list "data" that is used for the graph
     app.agregated_retweets.forEach(function (retweet) {
-        console.log(retweet.created_at + ", cnt:" + counter);
+        console.log('sumando ' + counter);
         data.push(
-
             {
                 x: retweet.created_at, y: counter
             }
@@ -164,27 +138,17 @@ let create_agregated_graph = function () {
         counter++;
     });
 
+    // Creating and displaying the graph
     var ctx = document.getElementById("canvas").getContext("2d");
     window.myLine = new Chart(ctx, config);
-
 }
-var data_two = [];
-
-let create_multiple_graph = function () {
-
-    var graph_two = document.getElementById("canvas_two").getContext("2d");
-    window.myLine = new Chart(graph_two, config_multiple_graph);
-};
-
-// var timeFormat = 'YYYY-MM-DDTHH:mm:ss.sssZ';
-var timeFormat = "ddd MMM dd HH:mm:ss Z yyyy"
 
 var config = {
     type: 'line',
     data: {
         datasets: [
             {
-                label: 'Date',
+                label: 'Date ',
                 data: data,
                 fill: true,
                 borderColor: '#212529'
@@ -245,18 +209,53 @@ var config = {
     }
 };
 
+// ____________________ MULTIPLE LINE GRAPH _______________________
+
+var dataset_multiline = [];
+
+let prepare_multiline_graph = function () {
+    // For each list of retweets, we must a dataset entry with all the retweets as data
+    // Therefore, each tweet's retweets are display as diferent values on the graph
+
+    // For each tweet, retrieve all the retweets and sotre them in the dataset_multiline for the graph
+    for (let i = 0; i < app.list_tweets_and_retweets.length; i++) {
+        let counter = 0;
+        let multiline_data = []
+        let retweets = app.list_tweets_and_retweets[i][1];
+        retweets = retweets.sort(function (a, b) { return (new Date(a.created_at) - new Date(b.created_at)).toString() });
+        retweets.forEach(function (retweet) {
+            // console.log(retweet.created_at + ", cnt:" + counter);
+            multiline_data.push(
+
+                {
+                    x: retweet.created_at, y: counter
+                }
+            );
+            counter++;
+        });
+
+        // data = app.list_tweets_and_retweets[i][1];
+        dataset_multiline.push({
+            label: 'Retweets ' + i,
+            data: multiline_data,
+            fill: true,
+            borderColor: '#212529'
+        });
+    }
+}
+
+let create_multiple_graph = function () {
+    // Isolate the preparation of the data in other method
+    prepare_multiline_graph();
+    var graph_two = document.getElementById("canvas_two").getContext("2d");
+    window.myLine = new Chart(graph_two, config_multiple_graph);
+};
+
 
 var config_multiple_graph = {
     type: 'line',
     data: {
-        datasets: [
-            {
-                label: "US Dates",
-                data: data_two,
-                fill: false,
-                borderColor: '#212529'
-            },
-        ]
+        datasets: dataset_multiline
     },
     options: {
         responsive: true,
@@ -312,6 +311,28 @@ var config_multiple_graph = {
     }
 };
 
+// ______________________ VUE COMPONENT ______________________
+
+let app = new Vue({
+    el: "#index",
+    delimiters: ['${', '}'],
+    unsafeDelimiters: ['!{', '}'],
+    data: {
+        label: 'none',
+        data_loaded: true,
+        word_search: 'casa',
+        tweets: [],
+        retweets: [],
+        top_users: [],
+        list_tweets_and_retweets: [],
+        agregated_retweets: []
+    },
+    methods: {
+        get_tweet: get_tweet,
+        save_data: save_data,
+        mark_as_fake: mark_as_fake
+    }
+});
 
 
 on_page_load();
